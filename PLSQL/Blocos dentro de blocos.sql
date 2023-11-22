e99f063ec29cc56cef5bce9d411067b79ed3802f
DavidNotebook <davidchristino@hotmail.com>
2023-11-22 13:53:13 -0300

    Blocos dentro de blocos

--- PLSQL/Blocos dentro de blocos.sql
+++ PLSQL/Blocos dentro de blocos.sql
@@ -3,6 +3,18 @@
  SET SERVEROUTPUT ON;
  
  DECLARE 
-       VBLOCO1 VARCHAR2(20) := 'Bloco 1';
+       VBLOCO1 VARCHAR2(20) := 'BANANA';
   BEGIN 
-       DBMS_OUTPUT.PUT_LINE ('Referenciando variavel ') 
+       DBMS_OUTPUT.PUT_LINE ('Referenciando variavel do bloco 1 =  ' || VBLOCO1);
+       DECLARE
+       VBLOCO2 VARCHAR(20) := 'Biscoito';
+       
+       BEGIN
+       
+       DBMS_OUTPUT.PUT_LINE ('Referenciando variavel do bloco 1 =  ' || VBLOCO1);
+       DBMS_OUTPUT.PUT_LINE ('Referenciando variavel do bloco 2 =  ' || VBLOCO2);
+       END;
+       -- SE CHAMAR VARIAVEIS QUE JÁ FORAM TERMINADAS EM BLOCOS ANTERIORES IRA DAR ERRO
+       DBMS_OUTPUT.PUT_LINE ('Referenciando variavel do bloco 1 =  ' || VBLOCO1);
+end;
+       
